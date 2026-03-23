1c4ded10d54d7e99576aab2763d7b1a219034654
Nik Sumeiko <nik.sumeiko@gmail.com>
2026-03-24 00:47:39 +0100

    feat: Add app heading title

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,15 @@ import { ProtectedRoute } from './features/auth/ProtectedRoute';
 function App() {
     return (
         <div className="min-h-screen bg-sky-100 flex flex-col">
-            <div className="flex justify-center pt-4">
+            <div className="flex flex-col items-center pt-4">
                 <img
                     src="/logo.png"
                     alt="Fhloston Paradise"
                     className="h-28"
                 />
+                <h1 className="mt-2 text-2xl font-semibold text-sky-900">
+                    Fhloston Paradise
+                </h1>
             </div>
             <Routes>
                 <Route
